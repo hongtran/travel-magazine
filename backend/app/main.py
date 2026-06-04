c95ad59922921e383cf337e1d34715e3082b9131
tranphihong <hongtran@tranphihongs-MacBook-Pro.local>
2026-06-04 21:20:38 +0700

    fix CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ app = FastAPI(title="Travel Magazine API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://travel-magazine-steel.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
